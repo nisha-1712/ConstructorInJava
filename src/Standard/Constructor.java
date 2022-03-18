dec1441e33958c75c271bf32e7445c93bf19c17e
Nisha Bhavsar <101039209+nisha-1712@users.noreply.github.com>
2022-03-18 18:15:35 +0530

    this is constructor file

---
+++ src/Standard/Constructor.java
@@ -0,0 +1,16 @@
+package Standard;
+
+public class Constructor {
+    int SchoolName;
+
+    public Constructor() {
+        System.out.println("My school name is SVVJ");
+
+    }
+
+    public static void main(String[] args) {
+        Constructor obj = new Constructor();
+
+    }
+
+}
