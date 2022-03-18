1cd61ce2e8a0f0d909b217a9352f1c3da60ce546
Nisha Bhavsar <101039209+nisha-1712@users.noreply.github.com>
2022-03-18 18:53:23 +0530

    Add School city name

--- src/Standard/Constructor.java
+++ src/Standard/Constructor.java
@@ -2,9 +2,11 @@ package Standard;
 
 public class Constructor {
     int SchoolName;
+    String SchoolCity;
 
     public Constructor() {
         System.out.println("My school name is SVVJ");
+        System.out.println("My School city is Pune");
 
     }
 
